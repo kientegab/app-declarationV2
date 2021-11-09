e9d9a70ab85470b44946406d890a459edfd90709
HEBIE <fat19ebie@gmail.com>
2021-11-09 12:48:00 +0000

    initial commit

---
+++ src/main/java/com/mfptps/appdgessddi/AppDgessddiApplication.java
@@ -0,0 +1,13 @@
+package com.mfptps.appdgessddi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDgessddiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDgessddiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mfptps/appdgessddi/AppDgessddiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfptps.appdgessddi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDgessddiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
