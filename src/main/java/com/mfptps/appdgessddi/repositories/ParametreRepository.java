0191c13bce6251b82e342532eee34dd105a09b13
ZINA JudeCarlos <zina.judecarlos@yahoo.fr>
2021-11-13 00:29:16 +0000

    gestion des parametres

---
+++ src/main/java/com/mfptps/appdgessddi/repositories/ParametreRepository.java
@@ -0,0 +1,9 @@
+package com.mfptps.appdgessddi.repositories;
+
+import com.mfptps.appdgessddi.entities.Parametre;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParametreRepository extends JpaRepository<Parametre, Long> {
+
+
+}
