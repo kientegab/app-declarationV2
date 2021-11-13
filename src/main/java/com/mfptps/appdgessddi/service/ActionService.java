7fe42d1087b8744594d1d989f21543be75d599fa
ZINA JudeCarlos <zina.judecarlos@yahoo.fr>
2021-11-13 17:20:03 +0000

    gestion des actions+objectif+parametre

---
+++ src/main/java/com/mfptps/appdgessddi/service/ActionService.java
@@ -0,0 +1,24 @@
+package com.mfptps.appdgessddi.service;
+
+import com.mfptps.appdgessddi.entities.Action;
+import com.mfptps.appdgessddi.service.dto.ActionDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+public interface ActionService {
+    Action create(ActionDTO actionDTO);
+
+    Action update(Action action);
+
+    Optional<Action> get(Long id);
+
+    List<Action> rechercheLibelle(String libelle);
+
+    Page<Action> findAll(Pageable pageable);
+
+    void delete(Long id);
+}
